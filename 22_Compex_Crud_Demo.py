8700c9d6ed93383faca8e8af58e14e5622cc5ea7
unknown <you@example.com>
2022-06-18 18:13:15 +0530

    Session 3 Files

--- 22_Compex_Crud_Demo.py
+++ 22_Compex_Crud_Demo.py
@@ -1,6 +1,7 @@
 import Utilities as util
 import time
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select # type: ignore
 
 browser = util.getChromeDriver()
 browser.get("http://127.0.0.1:8000/admin")
@@ -18,16 +19,42 @@ submitButton.click()
 time.sleep(5)
 
 ####################### CRUD #######################
-regionLink = browser.find_element(By.CSS_SELECTOR, '[href="/admin/Lists/regions/"]')
-regionLink.click()
+productLink = browser.find_element(By.CSS_SELECTOR, '[href="/admin/Masters/products/"]')
+productLink.click()
 time.sleep(3)
 
-addRegionLink = browser.find_element(By.PARTIAL_LINK_TEXT, 'ADD REGION')
-addRegionLink.click()
+addProdcutLink = browser.find_element(By.PARTIAL_LINK_TEXT, 'ADD PRODUCT')
+addProdcutLink.click()
 time.sleep(3)
 
-regionDescInput = browser.find_element(By.NAME, "RegionDescription")
-regionDescInput.send_keys("Test Region")
+regionDescInput = browser.find_element(By.NAME, "ProductName")
+regionDescInput.send_keys("Marie Gold")
+
+supplierInput = browser.find_element(By.NAME, "Supplier")
+supplierSelect = Select(supplierInput)
+supplierSelect.select_by_visible_text("Ma Maison")
+
+categoryInput = browser.find_element(By.NAME, "Category")
+categorySelect = Select(categoryInput)
+categorySelect.select_by_visible_text("Confections")
+
+quantityPerUnitInput = browser.find_element(By.NAME, "QuantityPerUnit")
+quantityPerUnitInput.send_keys("1")
+
+unitPriceInput = browser.find_element(By.NAME, "UnitPrice")
+unitPriceInput.send_keys("8.75")
+
+unitsInStockInput = browser.find_element(By.NAME, "UnitsInStock")
+unitsInStockInput.send_keys("10")
+
+unitsOnOrderInput = browser.find_element(By.NAME, "UnitsOnOrder")
+unitsOnOrderInput.send_keys("60")
+
+reorderLevelInput = browser.find_element(By.NAME, "ReorderLevel")
+reorderLevelInput.send_keys(1)
+
+discontinuedInput = browser.find_element(By.NAME, "Discontinued")
+discontinuedInput.click()
 
 saveButton = browser.find_element(By.CSS_SELECTOR, '[value="Save"]')
 saveButton.click()
